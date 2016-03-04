83a5b54d2a9f42aa1a9b8be7816939858a3941a5
Joshua Roberts <joshuapr1@gmail.com>
2016-03-04 12:57:58 -0600

    First commit adding webapp

---
+++ app/index.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+import simplejson as json
+from urllib import urlopen
+
+@app.route('/')
+def index():
+	api_endpoint = urlopen('https://api.indiegogo.com/1/search/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346').read()
+	items = json.loads(api_endpoint)['response']
+	return render_template('index.html', **locals())
+
+
+
+
+
